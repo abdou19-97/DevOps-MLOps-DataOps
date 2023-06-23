45c59cb328154f7adef8c5d4e7b3b7598b5850f4
abdou19-97 <61571968+abdou19-97@users.noreply.github.com>
2023-06-23 00:20:23 +0000

    Adding initail layout

---
+++ hello.py
@@ -0,0 +1,14 @@
+def more_hello():
+    return "Hello:))"
+
+
+def more_goodbye():
+    return "Bye!!"
+
+
+# This a nonsense code that generate warning
+# var = 1
+# var = var
+
+# # Bad Syntax
+# foo =
